96f132fbd5a11d8ecd052ab5477b5b7b366b0be1
anamoraess02 <132961916+anamoraess02@users.noreply.github.com>
2023-05-10 14:25:42 -0300

    Project createad

---
+++ src/main/java/com/devsuperiorprojeto/Projeto/Java/Spring/ProjetoJavaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperiorprojeto.Projeto.Java.Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoJavaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoJavaSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperiorprojeto/Projeto/Java/Spring/ProjetoJavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperiorprojeto.Projeto.Java.Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoJavaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
